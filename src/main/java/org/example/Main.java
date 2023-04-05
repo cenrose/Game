5c528d5c204322283a4dab909a1ff8ed8b2e1806
cenrose <115883202+cenrose@users.noreply.github.com>
2023-04-05 12:02:46 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int randomInt = new Random().nextInt(1000); // Генерирует новое число от 0 до 1000
+
+        int userInput = -1; // Это нужно, чтобы цикл запустился, если Random выдаст 0
+        System.out.println("Я загадал число от 0 до 1000.");
+        System.out.println("Ваш ход:");
+
+        // Напишите условие цикла для запуска игры
+
+        while(userInput != randomInt){
+            userInput = scanner.nextInt(); // В этой переменной должен сохраняться ввод пользователя
+
+            if (userInput > randomInt) { // Условие проверяется в цикле
+                System.out.println("Меньше");
+            }else if(userInput < randomInt)
+                // Второе условие
+                System.out.println("Больше");
+        }
+
+        // Печатаем, когда число угадано
+        System.out.println("Вы великолепны! Именно это я загадал.");
+    }
+
+}
